42cbad06e865fd528d80b03c5972aadc57240beb
Nahely <yanez@gmail.com>
2022-08-24 17:22:03 -0400

    se agrego la suma y resta de dos numeros

---
+++ number.txt
@@ -0,0 +1,18 @@
+<?php
+function sumadosnumeros($x, $y)
+{
+$num1 = $x;
+$num2 = $y;
+$result = $x + $y;
+return $result;
+}
+print(sumadosnumeros (5, 6))
+function restadosnumeros($x, $y)
+{
+    $num1 = $x;
+    $num2 = $y;
+    $result = $x - $y;
+    return $result;
+    }
+    print(restadosnumeros (5, 6))
+?>
--- suma.php
+++ suma.php
@@ -1,35 +1,18 @@
-<!DOCTYPE HTML>
-<html lang="es-ES">
-<head>
-	<meta charset="UTF-8">
-	<title>Suma de 2 numeros enteros</title>
-    </head>
-<body>
-<H2>Suma de 2 numeros enteros</H2>
-<form action="suma.php" method="POST">
-	<table>
-	<tr>
-		<td><input type="text" name="num1"></td>
-	</tr>
-	<tr>
-		<td><input type="text" name="num2"></td>
-	</tr>
-	<tr>
-		<td> <input type="submit" value="sumar"> </td>
-	</tr>
-</table>
-</form>
-</body>
-</html>
 <?php
-	if($_POST)
-	{	
-		$num1 = $_POST
-		['num1'];
-		$num2 = $_POST
-		['num2'];
-		$suma = $num1 
-		+ $num2;
-		echo "La suma de ".$num1." y ".$num2." es ".$suma; 
-	}
+function sumadosnumeros($x, $y)
+{
+$num1 = $x;
+$num2 = $y;
+$result = $x + $y;
+return $result;
+}
+print(sumadosnumeros (5, 6))
+function restadosnumeros($x, $y)
+{
+    $num1 = $x;
+    $num2 = $y;
+    $result = $x - $y;
+    return $result;
+    }
+    print(restadosnumeros (5, 6))
 ?>
